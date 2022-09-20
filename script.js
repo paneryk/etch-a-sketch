ffd1a1dcc1efa8dd420c7a836d34ddc50ae17300
paneryk <eryk.wojdyla@gmail.com>
2022-09-20 13:50:37 +0200

    Added new functionality Option to select the number of squares per grid side and a button that resets the board

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const board = document.querySelector('#board');
 
 function createSquares(squareSize) { //creates .square divs of squareSize width and height inside #board div
+    squareSize = (480/squareSize);
     for (i=0; i<Math.pow((480/squareSize), 2); i++) {
     const square = document.createElement('div');
     square.classList.add('square');
@@ -11,16 +12,44 @@ function createSquares(squareSize) { //creates .square divs of squareSize width 
 };
 }
 
-createSquares(16); //creates squares of 16px size
+function listening() {
+    const squaresAll = document.querySelectorAll('div.square');
+    squaresAll.forEach((square) => {
+        square.addEventListener('mouseover', (e) => {
+            if (e.buttons == 1 || e.buttons == 3) {
+            square.classList.add('hover');
+            }
+        });
+    });
+    }
 
-const squaresAll = document.querySelectorAll('div.square');
-squaresAll.forEach((square) => {
-    square.addEventListener('mouseover', (e) => {
-        if (e.buttons == 1) {
-        square.classList.add('hover');
-        }
+function resetBoard() {
+    const squaresAll = document.querySelectorAll('div.square');
+    squaresAll.forEach((square) => {
+        square.classList.remove('hover');
     })
-})
+};
+
+const selectSizeButton = document.querySelector('#selectSizeButton');
+selectSizeButton.addEventListener(('click'), () => {
+    let squareSize = prompt(`Enter the number of squares per side of a grid`, "50");
+    const squaresAll = document.querySelectorAll('div.square');
+    squaresAll.forEach((square) => {
+        square.remove();
+    });
+    createSquares(squareSize);
+    listening();
+});
+
+const resetButton = document.querySelector('#resetButton');
+resetButton.addEventListener(('click'), resetBoard);
+
+createSquares(30);
+listening();
+
+
+
+
 
 
 
