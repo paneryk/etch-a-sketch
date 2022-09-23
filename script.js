9b5277cd9bcef62bf0ffa8c49628018798f8d133
paneryk <eryk.wojdyla@gmail.com>
2022-09-23 10:17:21 +0200

    Added several new functions and fixed some bugs The app is now fully functional with options to change the color, reset the board, change the grid size and with rainbow mode

--- script.js
+++ script.js
@@ -1,64 +1,72 @@
 const board = document.querySelector('#board');
+const controls = document.querySelector('.controls');
+const colorSelection = document.querySelector('input');
+let color = colorSelection.value;
+let rainbow = false;
 
-function createSquares(squareSize) { //creates .square divs of squareSize width and height inside #board div
+controls.addEventListener(('click'), function(e) {  //adding eventListener to the control buttons 
+    const tgt = e.target;
+    if (tgt.matches('#selectSizeButton')) selectSize();
+    else if (tgt.matches('#resetButton')) resetBoard();
+    else if (tgt.matches('#rainbowButton')) { rainbow = true; }
+})
+
+board.addEventListener('mouseover', function(e) {  //adding eventListener to the square divs (listening to entire board div)
+    if (e.buttons === 1 || e.buttons === 3) {
+        let tgt = e.target;
+        switch(tgt.style.backgroundColor) { 
+        case 'transparent':
+        color = rainbow ? "#" + ((1<<24)*Math.random() | 0).toString(16) : color; //ternary expression checks if rainbow is true
+        tgt.style.backgroundColor = color;
+        break;
+        default:
+        let RegExp = /\(([^)]+)\)/;
+        let RegExpMatch = RegExp.exec(tgt.style.filter); //get the raw current brightness value
+        tgt.style.filter = `brightness(${RegExpMatch[1]-0.2})`;
+        }      
+}
+});
+
+colorSelection.addEventListener(('input'), function() {
+    rainbow = false;
+    color = colorSelection.value;
+})
+
+function createSquares(squareSize) {  //function creating a grid of square.divs of a demanded size
     squareSize = (480/squareSize);
     for (i=0; i<Math.pow((480/squareSize), 2); i++) {
     const square = document.createElement('div');
     square.classList.add('square');
     square.style.cssText = `
         width: ${squareSize}px;
-        height: ${squareSize}px;`; //sets size of the .square divs
+        height: ${squareSize}px;
+        background-color: transparent;
+        filter: brightness(1);`; 
     board.appendChild(square);
 };
 }
-
-function listening(color) {
-    const squaresAll = document.querySelectorAll('div.square');
-    squaresAll.forEach((square) => {
-        square.addEventListener('mouseover', (e) => {
-            if (e.buttons == 1 || e.buttons == 3) {
-                switch(color) {
-                    case 'rainbow':
-                        let rainbowColor = "#" + ((1<<24)*Math.random() | 0).toString(16);
-                        e.target.style.backgroundColor = rainbowColor;
-                    default:
-                        e.target.style.backgroundColor = color; 
-                }
-
-            }
-        });
-    });
-    }
 
 function resetBoard() {
     const squaresAll = document.querySelectorAll('div.square');
     squaresAll.forEach((square) => {
         square.style.backgroundColor = 'transparent';
+        square.style.filter = 'brightness(1)';
     })
-    listening('black');
-};
+}
 
-const selectSizeButton = document.querySelector('#selectSizeButton');
-selectSizeButton.addEventListener(('click'), () => {
-    let squareSize = prompt(`Enter the number of squares per side of a grid`, "50");
+function selectSize() {
+    let squareSize = prompt(`Enter the number of squares per side of a grid (max. 100)`, "50");
+    if (squareSize > 100) alert('Maximum number is 100, try again');
+    else {
     const squaresAll = document.querySelectorAll('div.square');
     squaresAll.forEach((square) => {
         square.remove();
     });
     createSquares(squareSize);
-    listening('black');
-});
+}
+}
 
-const resetButton = document.querySelector('#resetButton');
-resetButton.addEventListener(('click'), resetBoard);
-
-const rainbowButton = document.querySelector('#rainbowButton');
-rainbowButton.addEventListener(('click'), () => listening('rainbow'));
-
-createSquares(30);
-listening('black');
-
-
+window.onload = createSquares(10);
 
 
 
